3519d09fb9fefa522e3fc33e53ca5f4006e1920c
OsamaB <osama.benhabara@live.se>
2015-10-15 15:41:53 +0200

    Create login-factory

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,16 +1,16 @@
 'use strict';
 angular.module('trellocloneApp')
-	.controller('LoginCtrl', function ($scope) {
+	.controller('LoginCtrl', ['$scope', 'loginFactory', function ($scope, loginFactory) {
 		$scope.username;
 		$scope.password;
 
 		$scope.validate = function (username, password) {
-			if (username === 'hej' && password === 'bajs') {
-				//redirect to board
-				window.alert('Correct input!');
-				window.location.replace("http://localhost:9000/#/");
-			} else {
-				window.alert('Wrong input!');
-			}
+			loginFactory.tryLogin(username, password)
+				.success(function (token) {
+					window.alert(token);
+				})
+				.error(function (error) {
+					window.alert(error);
+				});
 		};
-	});
+	}]);
---
+++ app/scripts/factories/login.js
@@ -0,0 +1,14 @@
+'use strict';
+angular.module('trellocloneApp')
+	.factory('loginFactory', ['$http', function ($http) {
+
+		var urlBase = 'http://localhost:8080/taskboard-web/login';
+		var loginFactory = {};
+		var loginObject = {};
+
+		loginFactory.tryLogin = function (username, password) {
+			loginObject.username = username;
+			loginObject.password = password;
+			return $http.post(urlBase, loginObject);
+		};
+	}]);
